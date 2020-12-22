58d965ee159604c290c7886e233d18bb10cbf8de
kisik <wis1906@naver.com>
2020-12-22 19:54:58 +0900

    remove for awhile

--- SphericalFPS/Source/SphericalFPS/SphericalFPS.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "SphericalFPS.h"
-#include "Modules/ModuleManager.h"
-
-DEFINE_LOG_CATEGORY(SphericalFPS);
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SphericalFPS, "SphericalFPS" );
